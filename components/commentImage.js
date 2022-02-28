ece5c843bc6536471916feebbe7dde75b3907046
Shubham <Shubham20015>
2022-02-28 15:03:44 +0530

    Finalize UI with some changes

--- components/commentImage.js
+++ components/commentImage.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
 
-function CommentImage() {
+function CommentImage({ size }) {
   return (
-    <div className="w-[120px] md:w-[80px]">
+    <div
+      className={
+        size == "headerImage"
+          ? "w-[7.5rem] md:w-[5rem]"
+          : "w-[5rem] md:w-[4rem]"
+      }
+    >
       <Image
         src={
           "https://images.unsplash.com/photo-1644424235841-bbd3a2c823c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
