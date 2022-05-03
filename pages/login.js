1ab82ced8daa2022c174851dd60409917625afea
Shubham <Shubham20015>
2022-05-03 23:24:10 +0530

    Fix login UI issue

--- pages/login.js
+++ pages/login.js
@@ -89,99 +89,97 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex justify-center items-center overflow-hidden min-h-screen bg-gradient-to-l from-[#0ED2F7] to-[#094FFF]">
+      <div className="flex justify-center items-center overflow-hidden h-screen bg-gradient-to-l from-[#0ED2F7] to-[#094FFF]">
         <Head>
           <title>Login Page</title>
         </Head>
         <ToastContainer position="top-right" autoClose={3000} />
-        <div className="flex flex-row justify-center items-center bg-white w-11/12 lg:w-3/5 xl:h-1/2 shadow-2xl rounded-xl font-dm">
-          <div className="md:flex flex-row md:justify-between ">
-            <div className="flex flex-col grow relative justify-between">
-              <form
-                className="flex flex-col items-center w-full md:w-full p-5 relative"
-                onSubmit={loginUser}
-              >
-                <a href="/" className="flex justify-center items-center">
-                  <img
-                    src="/images/logo.png"
-                    alt=""
-                    className="w-3/5 md:w-4/5 mt-3"
+        <div className="flex justify-center items-center bg-white w-11/12 lg:w-3/5 shadow-2xl rounded-xl font-dm h-5/6">
+          <div className="flex flex-col grow justify-between w-2/5 h-full">
+            <form
+              className="flex flex-col items-center w-full md:w-full p-5 mt-5 md:mt-0"
+              onSubmit={loginUser}
+            >
+              <a href="/" className="flex justify-center items-center">
+                <img
+                  src="/images/logo.png"
+                  alt=""
+                  className="w-2/3 md:w-4/5 mt-3"
+                />
+              </a>
+              <h1 className="font-bold text-xl md:text-xl">
+                Login to your Account
+              </h1>
+              <div className="p-4 m-6 text-center space-y-5">
+                <div className="relative border-solid border border-[#BCBCBC] rounded-md p-1 space-x-9">
+                  <Icon
+                    icon="cil:send"
+                    color="#ccc"
+                    className="pointer-events-none  w-6 h-6 absolute"
                   />
-                </a>
-                <h1 className="font-bold text-2xl md:text-xl">
-                  Login to your Account
-                </h1>
-                <div className="p-4 m-6 text-center space-y-5">
-                  <div className="relative border-solid border border-[#BCBCBC] rounded-md p-1 space-x-9">
-                    <Icon
-                      icon="cil:send"
-                      color="#ccc"
-                      className="pointer-events-none  w-6 h-6 absolute"
-                    />
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Your Email"
-                      className="ml-5 focus:outline-none"
-                    />
-                  </div>
-
-                  <div className="relative border-solid border border-[#BCBCBC] rounded-md p-1 space-x-9">
-                    <Icon
-                      icon="bx:lock-alt"
-                      color="#ccc"
-                      className="pointer-events-none w-6 h-6 absolute text-gray-300"
-                    />
-                    <input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      name="password"
-                      id="password"
-                      placeholder="Your Password"
-                      className="ml-5 focus:outline-none"
-                    />
-                  </div>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Your Email"
+                    className="ml-5 focus:outline-none"
+                  />
                 </div>
+
+                <div className="relative border-solid border border-[#BCBCBC] rounded-md p-1 space-x-9">
+                  <Icon
+                    icon="bx:lock-alt"
+                    color="#ccc"
+                    className="pointer-events-none w-6 h-6 absolute text-gray-300"
+                  />
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="password"
+                    id="password"
+                    placeholder="Your Password"
+                    className="ml-5 focus:outline-none"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col justify-center items-center mb-20">
                 <input
                   type="submit"
                   value="Login"
-                  className="bg-[#3770FF] w-3/4 text-white p-2 rounded-lg cursor-pointer hover:bg-[#2160ff]"
+                  className="bg-[#3770FF] w-full text-white p-2 rounded-lg cursor-pointer hover:bg-[#2160ff]"
                 />
-                <p className="text-gray-400 mt-4">OR</p>
-                <div className="flex justify-center items-center mb-20">
-                  <GoogleLogin
-                    clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
-                    buttonText="Continue With Google"
-                    className="googleButton"
-                    onSuccess={handleLogin}
-                    onFailure={handleFailure}
-                    cookiePolicy={"single_host_origin"}
-                  ></GoogleLogin>
-                </div>
-              </form>
-              {/* <div className="flex justify-center items-center">
+                <p className="text-gray-400 m-4">OR</p>
+                <GoogleLogin
+                  clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+                  buttonText="Continue With Google"
+                  className="googleButton"
+                  onSuccess={handleLogin}
+                  onFailure={handleFailure}
+                  cookiePolicy={"single_host_origin"}
+                ></GoogleLogin>
+              </div>
+            </form>
+            {/* <div className="flex justify-center items-center">
                 <button className="flex  space-x-2 mt-3 border-solid border border-[#DFD7D7] rounded-md p-2 mb-20 hover:bg-gray-100 justify-center w-3/5 items-center ">
                   <Icon icon="flat-color-icons:google" className="w-6 h-6" />
                   <p>Continue with Google</p>
                 </button>
               </div> */}
-              <Link href="/signup">
-                <button className="bg-[#F6F6F6] text-[#3770FF] font-semibold w-full p-3 rounded-md shadow-lg shadow-blue-500">
-                  Not having an account? Sign Up
-                </button>
-              </Link>
-            </div>
-            <div className="hidden md:block w-3/5 ">
-              <img
-                src="/images/thumbnail.svg"
-                className="h-full object-cover rounded-br-xl"
-                alt=""
-              />
-            </div>
+            <Link href="/signup">
+              <button className="bg-[#F6F6F6] text-[#3770FF] font-semibold w-full p-3 text-sm md:text-base rounded-bl-xl rounded-br-xl shadow-lg shadow-blue-500">
+                Not having an account? Sign Up
+              </button>
+            </Link>
+          </div>
+          <div className="hidden md:block w-3/5 bg-green-200 h-full rounded-br-xl rounded-tr-xl">
+            <img
+              src="/images/thumbnail.svg"
+              className="h-full w-full object-cover rounded-br-xl rounded-tr-xl"
+              alt=""
+            />
           </div>
         </div>
       </div>
