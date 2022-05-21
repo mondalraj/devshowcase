611d286c5c067ef4ff424b0c92487d085d6e6334
Rajib Mondal <mondal021002rajib@gmail.com>
2022-05-21 18:34:17 +0530

    Send message functionality complete done on profile page

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -140,7 +140,7 @@ function profile() {
           toName={userData.name}
         />
       )}
-      <div className="profile_container font-dm">
+      <div className="profile_container min-h-screen font-dm">
         <div className="profile_navbar max-w-screen-xl mx-auto w-full h-16 flex justify-between items-center p-5">
           <a href="/">
             <img
