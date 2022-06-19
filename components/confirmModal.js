ac11422f23f6478eca1eb11555c032533f2a6f3a
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-06-19 16:36:30 +0530

    issues fixed

--- components/confirmModal.js
+++ components/confirmModal.js
@@ -37,7 +37,7 @@ export default function ConfirmModal({ message, setConfirm, cb }) {
             type="button"
             className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2 md:mr-8"
             onClick={() => {
-              setAns(true);
+              setConfirm(true);
               cb();
             }}
           >
